509eea4141d5c42f8264a8cba7f697cea82f0c9c
Vivelune <shafiqueeee.atif@gmail.com>
2025-12-14 10:08:12 +0800

    18: ExecutionNodes

---
+++ src/features/executions/components/http-request/executor.ts
@@ -0,0 +1,56 @@
+import type { NodeExecutor } from "@/features/executions/types";
+import { NonRetriableError } from "inngest";
+import ky, {type Options as KyOptions} from "ky"
+
+type HTTPRequestData = {
+    endpoint?: string;
+    method?: "GET" | "POST" | "PUT" | "PATCH"| "DELETE";
+    body?: string;
+}
+
+export const httpRequestExecutor: NodeExecutor<HTTPRequestData> = async({
+    data,
+    nodeId,
+    context, 
+    step,
+}) =>{
+    if(!data.endpoint){
+        throw new NonRetriableError("HTTP Request Node is missing endpoint configuration.")
+    }
+
+
+    const result = await step.run("http-request", async ()=> {
+        const endpoint = data.endpoint!;
+        const method = data.method || "GET";
+
+         const options: KyOptions = {method};
+
+         if (["POST", "PUT", "PATCH"].includes(method)){
+            
+            options.body = data.body;
+            
+        }
+        const response = await ky(endpoint, options);
+        const contentType = response.headers.get("content-type");
+        const responseData =  contentType?.includes("application/json")
+            ? await response.json()
+            : await response.text();
+
+        return{
+            ...context,
+            httpResponse:{
+                status: response.status,
+                statusText: response.statusText,
+              data: responseData,
+
+            }
+       }
+     })
+
+    // const result = await step.fetch(data.endpoint);
+
+    // Publish loading state for http request
+    // const result = await step.run("http-request", async ()=>context)
+
+    return result
+}
