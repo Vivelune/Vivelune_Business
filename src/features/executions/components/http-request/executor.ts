3735279df298a10adcee368e807a3c33a774b108
Vivelune <shafiqueeee.atif@gmail.com>
2025-12-16 05:20:22 +0800

    20:Node Templating

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,11 +1,18 @@
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, {type Options as KyOptions} from "ky"
+import Handlebars from "handlebars";
+
+Handlebars.registerHelper("json" , (context) => {
+    const jsonString = JSON.stringify(context, null , 2);
+    const safeString = new Handlebars.SafeString(jsonString);
+    return safeString;
+})
 
 type HTTPRequestData = {
-    variableName?: string;
-    endpoint?: string;
-    method?: "GET" | "POST" | "PUT" | "PATCH"| "DELETE";
+    variableName: string;
+    endpoint: string;
+    method: "GET" | "POST" | "PUT" | "PATCH"| "DELETE";
     body?: string;
 }
 
@@ -24,16 +31,24 @@ export const httpRequestExecutor: NodeExecutor<HTTPRequestData> = async({
     }
 
 
+    if(!data.method){
+        throw new NonRetriableError("HTTP Request Node is missing method configuration.")
+    }
+
 
     const result = await step.run("http-request", async ()=> {
-        const endpoint = data.endpoint!;
-        const method = data.method || "GET";
+        const endpoint = Handlebars.compile(data.endpoint)(context);
+
+
+    console.log("ENDPOINT", {endpoint});
+        const method = data.method;
 
          const options: KyOptions = {method};
 
          if (["POST", "PUT", "PATCH"].includes(method)){
-            
-            options.body = data.body;
+            const resolved = Handlebars.compile(data.body || "{}")(context)
+            JSON.parse(resolved);
+            options.body = resolved;
             options.headers={
                 "Content-Type": "application/json",
             }
@@ -54,16 +69,12 @@ export const httpRequestExecutor: NodeExecutor<HTTPRequestData> = async({
 
             }
             }
-            if(data.variableName){
+          
             return{
             ...context,
             [data.variableName] : responePayLoad
             }
-            }
-            return{
-                ...context,
-                ...responePayLoad,
-            }
+           
         
      })
 
