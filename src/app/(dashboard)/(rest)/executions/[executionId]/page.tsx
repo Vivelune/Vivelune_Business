2959c43f58ab00545887c554041d27f78d27e850
Vivelune <shafiqueeee.atif@gmail.com>
2025-12-21 09:44:24 +0800

    27:Executions History

--- src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
+++ src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
@@ -1,4 +1,10 @@
+import { ExecutionView } from "@/features/executions/components/execution";
+import { ExecutionsError, ExecutionsLoading } from "@/features/executions/components/executions";
+import { prefetchExecution } from "@/features/executions/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient, prefetch } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface PageProps{
     params:Promise<{executionId:string}>
@@ -9,7 +15,23 @@ interface PageProps{
 const Page = async ({params}:PageProps) => {    
     await requireAuth();
     const {executionId}= await params;
-    return <div>ExecutionId : {executionId}</div>;   
+    prefetchExecution(executionId)
+
+
+    return (
+        <div className="p-4 md:px-10 py-6 h-full">
+            <div className="mx-auto max-w-3xl w-full flex flex-col gap-y-8 h-full">
+                <HydrateClient>
+                    <ErrorBoundary fallback={<ExecutionsError/>}>
+                        <Suspense fallback={<ExecutionsLoading/>}>
+                            <ExecutionView executionId={executionId}/> 
+                        </Suspense>
+                    </ErrorBoundary>
+                </HydrateClient>
+            </div>
+
+        </div>
+    )
 };
   
 export default Page;
--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -2,6 +2,7 @@ import toposort from "toposort";
 import { Connection } from "@/generated/prisma/client";
 import { Node } from "@/generated/prisma/client";
 import { inngest } from "./client";
+import { createId } from "@paralleldrive/cuid2";
 
 
 export const topologicalSort = (
@@ -64,5 +65,6 @@ export const sendWorkflowExecution = async (data: {
     return  inngest.send({
             name:"workflows/execute.workflow",
             data,
+            id: createId(),
         })
 }
