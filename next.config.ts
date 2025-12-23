637058828b000c14732f071e1a117bf94f841149
Vivelune <shafiqueeee.atif@gmail.com>
2025-12-23 13:00:45 +0800

    Change the directry of page, change the redirect of initial launch

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,8 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source:"/workflows",
-        destination:"/",
+        source:"/",
+        destination:"/landingpage",
         permanent:false,
       }
     ]
