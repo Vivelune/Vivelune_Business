c7b293139c5e33badd17158ce7d69658570922f0
Vivelune <shafiqueeee.atif@gmail.com>
2025-12-23 12:16:20 +0800

    Polar Email Verification Remover

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,18 +12,18 @@ export const auth = betterAuth({
   
   emailAndPassword:{
     enabled: true,
-    requireEmailVerification: true,
+    // requireEmailVerification: true,
 
   },
-  emailVerification: {
-    sendVerificationEmail: async ( { user, url }) => {
-      await sendEmail({
-        to: user.email,
-        subject: "Verify your email address",
-        text: `Click the link to verify your email: ${url}`,
-      });
-    },
-  },
+  // emailVerification: {
+  //   sendVerificationEmail: async ( { user, url }) => {
+  //     await sendEmail({
+  //       to: user.email,
+  //       subject: "Verify your email address",
+  //       text: `Click the link to verify your email: ${url}`,
+  //     });
+  //   },
+  // },
   socialProviders: {
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID as string, 
