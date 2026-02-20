ec351758d10bdabcffd52f7b5db8816ae84774d3
Vivelune <shafiqueeee.atif@gmail.com>
2026-02-20 13:41:09 +0800

    Sandbox and Development dynamic in Polar

--- src/lib/polar.ts
+++ src/lib/polar.ts
@@ -4,5 +4,5 @@ export const polarClient = new Polar({
     accessToken: process.env.NODE_ENV === "development" 
     ? process.env.POLAR_ACCESS_TOKEN_DEVELOPMENT
     : process.env.POLAR_ACCESS_TOKEN,
-    server:"sandbox", // TODO: CHANGE IN PRODUCTION
+    server:process.env.NODE_ENV === "development" ? "sandbox" : "production", // TODO: CHANGE IN PRODUCTION
 })
