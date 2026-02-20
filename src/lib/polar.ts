cf65aa2577975654e03ed28bff649a987eb4f39a
Vivelune <shafiqueeee.atif@gmail.com>
2026-02-20 13:50:42 +0800

    Check Polar Token

--- src/lib/polar.ts
+++ src/lib/polar.ts
@@ -1,8 +1,9 @@
 import {Polar} from "@polar-sh/sdk"
-
+console.log("POLAR TOKEN:", process.env.POLAR_ACCESS_TOKEN);
 export const polarClient = new Polar({
     accessToken: process.env.NODE_ENV === "development" 
-    ? process.env.POLAR_ACCESS_TOKEN_DEVELOPMENT
+    ? process.env.POLAR_ACCESS_TOKEN
     : process.env.POLAR_ACCESS_TOKEN,
+    
     server:process.env.NODE_ENV === "development" ? "sandbox" : "production", // TODO: CHANGE IN PRODUCTION
 })
