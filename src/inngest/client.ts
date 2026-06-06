0fdd3c92764e00b548133af25b8894bb55cbde90
Vivelune <shafiqueeee.atif@gmail.com>
2026-06-07 07:43:19 +0800

    Removed the realtime/Inngest

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,8 +1,7 @@
 import { Inngest } from "inngest";
-import {realtimeMiddleware} from "@inngest/realtime/middleware"
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ 
     id: "vivelune",
-    middleware:[realtimeMiddleware()]
+    // Look, no hands! The middleware is handled natively now.
 });
